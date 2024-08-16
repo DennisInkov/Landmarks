a65fc6824579b9cc19af8d568ebcc5dbd8500731
DennisInkov <135211473+DennisInkov@users.noreply.github.com>
2024-08-16 18:23:01 +0500

    Define a Landmark structure with a few properties matching names of some of the keys in the landmarkData data file.
    Adding Codable conformance makes it easier to move data between the structure and a data file. You’ll rely on the Decodable component of the Codable protocol later in this section to read data from file.

---
+++ Landmarks/Preview Content/Landmark.swift
@@ -0,0 +1,18 @@
+//
+//  Landmark.swift
+//  Landmarks
+//
+//  Created by Dennis on 16.08.2024.
+//
+
+import Foundation
+import SwiftUI
+
+struct Landmark: Hashable, Codable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var Description: String
+    
+}
