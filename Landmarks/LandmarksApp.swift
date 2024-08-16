4345620c957810869d2b8fc4c8eab6c3e27d60e4
DennisInkov <135211473+DennisInkov@users.noreply.github.com>
2024-08-16 17:40:56 +0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Dennis on 16.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
